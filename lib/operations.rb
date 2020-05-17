1d53aae339bb8f488e9c350000efdd2bd99472b7
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-17 16:27:57 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
-
+if speed <40 || speed>60
+  return true
+else 
+  return false
 end
 
 
+def unsafe?(speed)
+  if speed > 60 
+    true
+  elsif speed < 40
+    true
+  else
+    false
+  end
 
-def not_safe?(speed)
-	
-end
-	
 
-
+speed <40 || speed >60 ? true : false 
